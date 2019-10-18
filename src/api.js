854fbdfb235678f4dfdbcee8cf684ed69bd3b137
wille <w.sandstrom@gmail.com>
2019-10-18 12:29:55 +0200

    update for bitrefill embed endpoint

--- src/api.js
+++ src/api.js
@@ -1,11 +1,12 @@
 // @flow
 export const PROVIDER = 'edge'
 export const API_VERSION = '1'
-export const BIT_REFILL_URL = 'https://www.bitrefill.com/'
+export const BITREFILL_URL = 'https://embed.bitrefill.com/'
 export const CALLBACK_KEY = 'edge'
 export const RETURN_URL = null
 export const KEYS = {
-  apiKey: ''
+  apiKey: '',
+  refToken: ''
 }
 
 export const SUPPORTED_DIGITAL_CURRENCIES = [
@@ -15,16 +16,14 @@ export const SUPPORTED_DIGITAL_CURRENCIES = [
   'DASH',
   'DOGE'
 ]
-// `https://www.bitrefill.com/embed/:coin?apiKey=&refundAddress=&paymentUriProtocol=edge
+// `https://embed.bitrefill.com/:coin?ref=&refundAddress=
 export const formatUrlCall = (address: string, type: string) => {
   const route = type.replace('wallet:', 'embed/')
   const url =
-    BIT_REFILL_URL +
+    BITREFILL_URL +
     route +
-    '?apiKey=' +
-    KEYS.apiKey +
-    '&paymentUriProtocol=' +
-    CALLBACK_KEY +
+    '?ref=' +
+    KEYS.refToken +
     '&refundAddress=' +
     address
   return url
